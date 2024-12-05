67d46d778fb4a9cb5bc2ee3b912746883a51b545
tubaranasakar <tubaranasakar@hotmail.com>
2024-12-06 01:00:00 +0300

    RecyclerView için gerekli tüm kodlamalar tamamlandı.

--- app/src/main/java/com/example/kisileruygulamasi/ui/adapter/KisilerAdapter.kt
+++ app/src/main/java/com/example/kisileruygulamasi/ui/adapter/KisilerAdapter.kt
@@ -1,12 +1,16 @@
 package com.example.kisileruygulamasi.ui.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kisileruygulamasi.data.entity.Kisiler
 import com.example.kisileruygulamasi.databinding.CardTasarimBinding
 import com.example.kisileruygulamasi.databinding.FragmentAnaSayfaBinding
+import com.example.kisileruygulamasi.ui.fragment.AnaSayfaFragmentDirections
+import com.google.android.material.snackbar.Snackbar
 
 class KisilerAdapter(var mContext:Context,var kisilerListesi:List<Kisiler>) : RecyclerView.Adapter<KisilerAdapter.CardTasarimTutucu>() {
 
@@ -14,18 +18,41 @@ class KisilerAdapter(var mContext:Context,var kisilerListesi:List<Kisiler>) : Re
     inner class CardTasarimTutucu(var tasarim:CardTasarimBinding) : RecyclerView.ViewHolder(tasarim.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimTutucu {
-        //viewBinding kurulumunun buraa yapılması gerekir
+        // ViewBinding kurulumunun burada yapılması gerekir
         val binding = CardTasarimBinding.inflate(LayoutInflater.from(mContext),parent,false)
         return CardTasarimTutucu(binding)
 
     }
 
     override fun onBindViewHolder(holder: CardTasarimTutucu, position: Int) {
+        // Asıl önemli kısım burasıdır. Yapılacak tüm işlemler burada gerçekleştirilir.
+        val kisi = kisilerListesi.get(position)
+        val t = holder.tasarim
+
+        t.textViewKisiAd.text = kisi.kisi_ad
+        t.textViewKisiTel.text = kisi.kisi_tel
+
+        t.cardViewSatir.setOnClickListener {
+            val gecis = AnaSayfaFragmentDirections.kisiDetayGecis(kisi = kisi)
+            Navigation.findNavController(it).navigate(gecis)
+        }
+
+        t.imageViewSil.setOnClickListener {
+            Snackbar.make(it,"${kisi.kisi_ad} silinsin mi? ",Snackbar.LENGTH_SHORT)
+                .setAction("EVET"){
+                    sil(kisi.kisi_id.toString())
+                }
+                .show()
+        }
     }
 
-    override fun getItemCount(): Int { //kaç tane item olacak burada belirtilir
+    override fun getItemCount(): Int {
+        //Kaç tane item olacak burada belirtilir
         return kisilerListesi.size
+    }
 
+    fun sil(kisi_Id:String){
+        Log.e("Kişi Sil",kisi_Id.toString())
     }
 
 }
--- app/src/main/java/com/example/kisileruygulamasi/ui/fragment/AnaSayfaFragment.kt
+++ app/src/main/java/com/example/kisileruygulamasi/ui/fragment/AnaSayfaFragment.kt
@@ -8,13 +8,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView.OnQueryTextListener
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kisileruygulamasi.R
+import com.example.kisileruygulamasi.data.entity.Kisiler
 import com.example.kisileruygulamasi.databinding.FragmentAnaSayfaBinding
+import com.example.kisileruygulamasi.ui.adapter.KisilerAdapter
 
 class AnaSayfaFragment : Fragment() {
     private lateinit var binding: FragmentAnaSayfaBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentAnaSayfaBinding.inflate(inflater, container, false)
+
+        binding.toolbarAnasayfa.title = "Kişiler"
+
+        binding.rv.layoutManager = LinearLayoutManager(requireContext()) //Listelenmenin alt alta görünmesini sağlar
+
+        val kisilerListesi = ArrayList<Kisiler>()
+        val k1 = Kisiler(1,"Tuba","1111")
+        val k2 = Kisiler(2,"Maris","2222")
+        val k3 = Kisiler(3,"Samet","3333")
+        kisilerListesi.add(k1)
+        kisilerListesi.add(k2)
+        kisilerListesi.add(k3)
+
+        val kisilerAdapter = KisilerAdapter(requireContext(),kisilerListesi)
+        binding.rv.adapter = kisilerAdapter //Listeyi görüntülemeyi sağlar
 
         binding.fab.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.kisiKayitGecis) //bağlantımız sayesinde KisiKayitFragmenta geçiş sağlandı
